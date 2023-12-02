7b4c1d439f61281fd178ede9313fac4a519cad23
MeHaVr <github@magicalex.org>
2023-12-03 00:14:52 +0100

    Auto Rolle

--- Lythia.py
+++ Lythia.py
@@ -39,6 +39,8 @@ async def on_member_join(member):
 
     channel = bot.get_channel(1180536176139059327)
 
+    #bild Generieren 
+
     Background = Editor("pic2.jpg")
     profile_image = await load_image_async(str(member.display_avatar.url))
 
@@ -48,12 +50,20 @@ async def on_member_join(member):
     poppins_small = Font.poppins(size=25, variant="light")
 
     Background.paste(profile, (325, 90))
-    Background.ellipse((325, 90), 150, 150, outline="white",stroke_width=5)
+    Background.ellipse((325, 90), 150, 150, outline="white",stroke_width=3)
 
     Background.text((400, 260), f"Willkommen auf {member.guild.name}", color="white", font=poppins, align="center")
     Background.text((400, 325), f"{member.name}", color="white", font=poppins_small, align="center")
 
     file = File(fp=Background.image_bytes, filename="pic1.jpg")
+
+    #Auto Rolle
+
+    role = discord.utils.get(member.guild.roles, name='Member')
+    await member.add_roles(role)
+    
+    #send Bild
+    
     await channel.send(f"Hello {member.mention}! Willkommen auf **{member.guild.name}** Lies dir bitte das #📚〣╠-regelwerk durch, damit keine Unannehmlichkeiten entstehen.")
     await channel.send(file=file)
 
