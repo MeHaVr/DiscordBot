a08cc1417d382a48def795f132b17b376b2421dd
MeHaVr <github@magicalex.org>
2023-12-19 11:14:34 +0100

    achievements reaction

--- cogs/achievements.py
+++ cogs/achievements.py
@@ -22,8 +22,8 @@ class Achievements(commands.Cog):
             self.conn = mariadb.connect(
                 user="root",
                 password=os.environ['DISCORD_DB_PASS'],
-                host="127.0.0.1",
-                port=13306,
+                host="Lythia.de",
+                port=3306,
                 database="discord"
             )
 
@@ -42,11 +42,11 @@ class Achievements(commands.Cog):
                 self.cur.execute("""CREATE TABLE `discord`.`achievements` (
                             `id` int AUTO_INCREMENT, PRIMARY KEY (id),
                             `user_id` varchar(128),
-                            `messages_count` int,
-                            `reaction_count` int,
-                            `images_count` int,
-                            `voice_channel_time` int,
-                            `event_count` int                    
+                            `messages_count` int default 0,
+                            `reaction_count` int default 0,
+                            `images_count` int default 0,
+                            `voice_channel_time` int default 0,
+                            `event_count` int default 0                    
                             );""")
                 print("table achievements got created.")
 
@@ -64,7 +64,7 @@ class Achievements(commands.Cog):
         if message.author.bot:
             return 
         
-
+        channel = bot.get_channel(1180536176139059327)
         self.cur.execute(f"select * from achievements where user_id = {message.author.id}")
         user = self.cur.fetchone()
         ic(user)
@@ -78,17 +78,47 @@ class Achievements(commands.Cog):
             self.conn.commit()
 
             if user[2]+1 == 10:
-                channel = bot.get_channel(1180536176139059327)
                 file = File(fp="cogs/img/Achievements/10nachrichten.png")
                 await channel.send(f"Herzliche Glückwunsch {message.author.mention} Sie haben {user[2]+1} Messages geschickt!", file=file)
             if user[2]+1 == 100:
-                channel = bot.get_channel(1180536176139059327)
                 file = File(fp="cogs/img/Achievements/100nachrichten.png")
                 await channel.send(f"Herzliche Glückwunsch {message.author.mention} Sie haben {user[2]+1} Messages geschickt!", file=file)
             if user[2]+1 == 1000:
-                channel = bot.get_channel(1180536176139059327)
                 file = File(fp="cogs/img/Achievements/1000nachrichten.png")
-                await channel.send(f"Herzliche Glückwunsch {message.author.mention} Sie haben {user[2]+1} Messages geschickt!", file=file)               
+                await channel.send(f"Herzliche Glückwunsch {message.author.mention} Sie haben {user[2]+1} Messages geschickt!", file=file)
+
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        channel = bot.get_channel(1180536176139059327)
+        async for user in reaction.users():
+            await channel.send(f'{user} has reacted with {reaction.emoji}!')
+         
+        channel = bot.get_channel(1180536176139059327)
+        self.cur.execute(f"select * from achievements where user_id = {user.id}")
+        user = self.cur.fetchone()
+        ic(user)
+        if user == None:
+            print("new user")
+            self.cur.execute(f"insert into achievements (user_id, messages_count) values ('{user.id}', 1);") 
+            self.conn.commit()
+        else:
+            print("updating message user")
+            self.cur.execute(f"update achievements set messages_count = {user[3]+1}")
+            self.conn.commit()
+
+            if user[3]+1 == 10:
+                file = File(fp="cogs/img/Achievements/10nachrichten.png")
+                await channel.send(f"Herzliche Glückwunsch {user.mention} Sie haben {user[3]+1} Reagierte geschickt!", file=file)
+            if user[3]+1 == 100:
+                file = File(fp="cogs/img/Achievements/100nachrichten.png")
+                await channel.send(f"Herzliche Glückwunsch {user.mention} Sie haben {user[3]+1} Reagierte geschickt!", file=file)
+            if user[3]+1 == 1000:
+                file = File(fp="cogs/img/Achievements/1000nachrichten.png")
+                await channel.send(f"Herzliche Glückwunsch {user.mention} Sie haben {user[3]+1} Reagierte geschickt!", file=file)
+
+
+                     
 
                 
 
--- requirements/install.bat
+++ requirements/install.bat
@@ -1,1 +1,1 @@
-pip install -r requirements.txt
+pip3.12 install -r requirements.txt
